67f68f7d5663982ae1c2520c02bef19f062449e1
이근선/선임연구원/B2B선행기술센터 위치측정솔루션Task(keunsun.lee@lge.com) <keunsun.lee@lge.com>
2023-05-31 17:53:09 +0900

    [modifying] sideBar layout

--- lib/widgets/rightSideBar_widget.dart
+++ lib/widgets/rightSideBar_widget.dart
@@ -8,63 +8,75 @@ class RightSideBarWidget extends StatelessWidget {
     final Size _size = MediaQuery.of(context).size;
     final _showDesktop = _size.width >= 1400;
 
-    return Expanded(
-      flex: 1,
-      child: ColoredBox(
-        color: const Color(0xFFCCECDF).withOpacity(0.5),
-        child: Container(
-          width: _showDesktop ? 400 : 0,
-          child: Row(
-            children: [
-              Expanded(
-                child: Container(
-                  height: 50,
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.white,
-                        width: 2.0,
+    return Column(
+      children: [
+        ColoredBox(
+          color: const Color(0xFFCCECDF).withOpacity(0.5),
+          child: Expanded(
+            child: Container(
+              width: _showDesktop ? 400 : 0,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                        ),
+                      ),
+                      child: const Center(
+                          child: Text(
+                        'Rull 목록',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      )),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                        ),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Rull 생성',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                  child: const Center(
-                      child: Text(
-                    'Rull 목록',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  )),
-                ),
+                ],
               ),
-              Expanded(
-                child: Container(
-                  height: 50,
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.white,
-                        width: 2.0,
-                      ),
-                    ),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Rull 생성',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
+        Container(
+          width: _showDesktop ? 400 : 0,
+          child: Text(
+            'list',
+            style: TextStyle(
+              fontSize: 48,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
